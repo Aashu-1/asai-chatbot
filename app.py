6ba2fd5101130707f6d43ab2e57ad2688e91ff79
Aashutosh Vibgyor <aashutosh.chouhan@vgos.org>
2026-03-21 12:21:58 +0530

    demo.launch updated

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ with gr.Blocks() as demo:
 
 if __name__ == "__main__":
     initialize_db()
-    demo.launch(show_api=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860, show_api=False)
